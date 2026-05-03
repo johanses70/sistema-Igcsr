93752045e4a27dae12f1f909bf02b156c1367b1d
johanses70 <stivinjr12@gmail.com>
2026-05-03 12:08:37 -0500

    Add custom exception classes for error handling

---
+++ excepciones.py
@@ -0,0 +1,11 @@
+class SistemaError(Exception):
+    pass
+
+class ClienteError(SistemaError):
+    pass
+
+class ServicioError(SistemaError):
+    pass
+
+class ReservaError(SistemaError):
+    pass
